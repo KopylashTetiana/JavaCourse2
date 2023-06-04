254534527732f492fd8da9c0c07dfc41ed4fb9e8
KopylashTetiana <kopylashtetiana@gmail.com>
2023-06-04 12:28:09 +0300

    MyThread2 class added, MyThread2.run() added instead off Ex2.run().

--- src/multithreading/Ex2.java
+++ src/multithreading/Ex2.java
@@ -1,31 +1,26 @@
 package multithreading;
 
 public class Ex2 extends Thread {
-    public void run() {
-        for(int i = 1000; i>=1; i--) {
-            System.out.println(i + "\"");
-        }
-    }
-    public static void main(String args[]) {
-        MyThread1 mTrd1 = new MyThread1();
-        Ex2 mTrd2 = new Ex2();
-        mTrd1.start();
-        mTrd2.start();
+    public static void main(String[] args) {
+        MyThread1 myThread1 = new MyThread1();
+        MyThread2 myThread2 = new MyThread2();
+        myThread1.start();
+        myThread2.start();
     }
 
 }
 class MyThread1 extends Thread {
     public void run() {
-        for(int i = 1; i <=1000; i++) {
-            System.out.println(i+"\'");
+        for(int i = 1; i <= 10000; i++) {
+            System.out.println(i+ "'");
         }
     }
 }
 
-//class MyThread2 extends Thread {
-//    public void run() {
-//        for(int i = 1000; i>=1; i--) {
-//            System.out.print(i+".");
-//        }
-//    }
-//}
+class MyThread2 extends Thread {
+    public void run() {
+        for(int i = 10000; i > 0; i--) {
+            System.out.println(i+"\"");
+        }
+    }
+}
